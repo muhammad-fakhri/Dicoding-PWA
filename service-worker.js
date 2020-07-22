9f815ce38366caddc8eefa22fd070f079131d0d3
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-07-22 20:40:49 +0700

    Turn off cache API

--- service-worker.js
+++ service-worker.js
@@ -31,53 +31,52 @@ const urlsToCache = [
 	'/js/init.js',
 	'/js/materialize.min.js',
 	'/js/script.js',
-	'/pages/about.html',
-	'/pages/contact.html',
+	'/pages/competition.html',
+	'/pages/favorite.html',
 	'/pages/home.html',
-	'/pages/service.html',
-	'/pages/work.html',
+	'/pages/team.html',
 	'https://code.jquery.com/jquery-2.1.1.min.js',
 	'https://fonts.googleapis.com/icon?family=Material+Icons',
 	'https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
-self.addEventListener('install', function (event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then(function (cache) {
-				return cache.addAll(urlsToCache);
-			})
-	);
-})
+// self.addEventListener('install', function (event) {
+// 	event.waitUntil(
+// 		caches.open(CACHE_NAME)
+// 			.then(function (cache) {
+// 				return cache.addAll(urlsToCache);
+// 			})
+// 	);
+// })
 
-self.addEventListener('activate', function (event) {
-	event.waitUntil(
-		caches.keys()
-			.then(function (cacheNames) {
-				return Promise.all(
-					cacheNames.map(function (cacheName) {
-						if (cacheName != CACHE_NAME) {
-							console.log("ServiceWorker: cache " + cacheName + " dihapus");
-							return caches.delete(cacheName);
-						}
-					})
-				);
-			})
-	);
-})
+// self.addEventListener('activate', function (event) {
+// 	event.waitUntil(
+// 		caches.keys()
+// 			.then(function (cacheNames) {
+// 				return Promise.all(
+// 					cacheNames.map(function (cacheName) {
+// 						if (cacheName != CACHE_NAME) {
+// 							console.log("ServiceWorker: cache " + cacheName + " dihapus");
+// 							return caches.delete(cacheName);
+// 						}
+// 					})
+// 				);
+// 			})
+// 	);
+// })
 
-self.addEventListener('fetch', function (event) {
-	event.respondWith(
-		caches.match(event.request, { cacheName: CACHE_NAME })
-			.then(function (response) {
-				if (response) {
-					console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-					return response;
-				}
+// self.addEventListener('fetch', function (event) {
+// 	event.respondWith(
+// 		caches.match(event.request, { cacheName: CACHE_NAME })
+// 			.then(function (response) {
+// 				if (response) {
+// 					console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+// 					return response;
+// 				}
 
-				console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
-				return fetch(event.request);
-			})
-	);
-});
+// 				console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+// 				return fetch(event.request);
+// 			})
+// 	);
+// });
 
