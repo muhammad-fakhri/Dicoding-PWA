cd05501e3ee65d121fa28790594e0cb7c8eb3219
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-07-25 18:50:41 +0700

    Add js file for testing push notification via FCM

---
+++ push.js
@@ -0,0 +1,30 @@
+import { setVapidDetails, sendNotification } from 'web-push';
+
+const vapidKeys = {
+    "publicKey": "BNyb8SYHYTaRBXDpKvqb8GrYpMkOcT5XyFdivoWIjTrcrhqaCAI2lyOMILxKTiEPDDV93jH65pGPSmQLELXBMXM",
+    "privateKey": "-sE2RPPPfN9u17sGWukxMtUJLXFpGlJ40DrbTIJNbnQ"
+};
+
+setVapidDetails(
+    'admin@muhammadfakhri.my.id',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+)
+const pushSubscription = {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/cH9sYkzRdAk:APA91bGNGZFxnY33CA6HlkXCp5EDLrmZuHqmwv6MloWcY1QXqyWLu4MkiaVbjvIldq4lfnFeTkxEy6ozponx_ItrWZ_X7YN6d_jT3ml2KSJCZ7wLX0Y5nE9VSoHF3x7C7taVVBHUTKYh",
+    "keys": {
+        "p256dh": "BEhye9O85wtEN3WG8s6rEKLUd6APJdLcLuq5aOv1lyj5NYAid/1/5VQ1Kf3EOTELitzd+i4EQIKPoytwVkNVCic=",
+        "auth": "P0CpnZRdUowp4WErYPsmWg=="
+    }
+};
+const payload = 'Hello! This is a push notification from Vanir!';
+
+const options = {
+    gcmAPIKey: '107891490415',
+    TTL: 60
+};
+sendNotification(
+    pushSubscription,
+    payload,
+    options
+);
