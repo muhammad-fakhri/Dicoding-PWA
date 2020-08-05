77d5d0442ced9984d5cac35fe6152ec38a10e095
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-05 15:25:29 +0700

    Update service worker to use workbox

--- service-worker.js
+++ service-worker.js
@@ -1,83 +1,47 @@
-const CACHE_NAME = 'vanir-v1';
-const urlsToCache = [
-	'/css',
-	'/images',
-	'/js',
-	'/pages',
-	'/favicon.ico',
-	'/index.html',
-	'/manifest.json',
-	'/nav.html',
-	'/service-worker.js',
-	'/css/materialize.min.css',
-	'/css/style.css',
-	'/images/icon',
-	'/images/background.jpg',
-	'/images/logo.png',
-	'/images/no-image.png',
-	'/images/icon/apple-touch-icon-180x180.png',
-	'/images/icon/favicon-16x16.png',
-	'/images/icon/favicon-32x32.png',
-	'/images/icon/pwa-192x192.png',
-	'/images/icon/pwa-512x512.png',
-	'/js/api.js',
-	'/js/db.js',
-	'/js/idb.js',
-	'/js/init.js',
-	'/js/installSW.js',
-	'/js/materialize.min.js',
-	'/js/nav.js',
-	'/pages/competition.html',
-	'/pages/developer.html',
-	'/pages/favorite.html',
-	'/pages/home.html',
-	'/pages/team.html',
-	'https://code.jquery.com/jquery-2.1.1.min.js',
-	'https://fonts.googleapis.com/icon?family=Material+Icons',
-	'https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-self.addEventListener('install', function (event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then(function (cache) {
-				return cache.addAll(urlsToCache);
-			})
-	);
-})
+workbox.precaching.precacheAndRoute([
+	{ url: '/css', revision: '1' },
+	{ url: '/images', revision: '1' },
+	{ url: '/js', revision: '1' },
+	{ url: '/pages', revision: '1' },
+	{ url: '/favicon.ico', revision: '1' },
+	{ url: '/index.html', revision: '1' },
+	{ url: '/manifest.json', revision: '1' },
+	{ url: '/nav.html', revision: '1' },
+	{ url: '/service-worker.js', revision: '1' },
+	{ url: '/css/materialize.min.css', revision: '1' },
+	{ url: '/css/style.css', revision: '1' },
+	{ url: '/images/icon', revision: '1' },
+	{ url: '/images/background.jpg', revision: '1' },
+	{ url: '/images/logo.png', revision: '1' },
+	{ url: '/images/no-image.png', revision: '1' },
+	{ url: '/images/icon/apple-touch-icon-180x180.png', revision: '1' },
+	{ url: '/images/icon/favicon-16x16.png', revision: '1' },
+	{ url: '/images/icon/favicon-32x32.png', revision: '1' },
+	{ url: '/images/icon/pwa-192x192.png', revision: '1' },
+	{ url: '/images/icon/pwa-512x512.png', revision: '1' },
+	{ url: '/js/api.js', revision: '1' },
+	{ url: '/js/db.js', revision: '1' },
+	{ url: '/js/idb.js', revision: '1' },
+	{ url: '/js/init.js', revision: '1' },
+	{ url: '/js/installSW.js', revision: '1' },
+	{ url: '/js/materialize.min.js', revision: '1' },
+	{ url: '/js/nav.js', revision: '1' },
+	{ url: '/pages/competition.html', revision: '1' },
+	{ url: '/pages/developer.html', revision: '1' },
+	{ url: '/pages/favorite.html', revision: '1' },
+	{ url: '/pages/home.html', revision: '1' },
+	{ url: '/pages/team.html', revision: '1' },
+	{ url: 'https://code.jquery.com/jquery-2.1.1.min.js', revision: '1' },
+	{ url: 'https://fonts.googleapis.com/icon?family=Material+Icons', revision: '1' },
+	{ url: 'https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2', revision: '1' },
+]);
 
-self.addEventListener('activate', function (event) {
-	event.waitUntil(
-		caches.keys()
-			.then(function (cacheNames) {
-				return Promise.all(
-					cacheNames.map(function (cacheName) {
-						if (cacheName != CACHE_NAME) {
-							console.log("ServiceWorker: cache " + cacheName + " deleted");
-							return caches.delete(cacheName);
-						}
-					})
-				);
-			})
-	);
-})
-
-self.addEventListener('fetch', function (event) {
-
-	event.respondWith(
-		caches.open(CACHE_NAME).then(function (cache) {
-			return cache.match(event.request).then(function (response) {
-				let fetchPromise = fetch(event.request).then(function (networkResponse) {
-					cache.put(event.request, networkResponse.clone());
-					return networkResponse;
-				})
-				return response || fetchPromise;
-			})
-		})
-
-
-	);
-});
+workbox.routing.registerRoute(
+	new RegExp('.*'),
+	workbox.strategies.staleWhileRevalidate()
+);
 
 self.addEventListener('push', function (event) {
 	let body;
